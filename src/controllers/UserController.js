f5804c3b51e8b5f605151e526d23638a20d17eea
NoeOn <alxg5516@gmail.com>
2023-12-31 14:49:43 -0600

    simplificando el codigo de la función registerUser del archivo UserController.js

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,17 +1,8 @@
 import User from '../models/UserModel.js';
 
 export async function registerUser(req, res) {
-
-    const { name, lastname, birthdate, email, password } = req.body;
     try {
-        const user = await User.create({
-            name,
-            lastname,
-            birthdate,
-            email,
-            password
-        });
-
+        const user = await User.create(req.body);
         res.status(200).json(user);
     } catch (error) {
         res.status(500).json({ message: error.message });
