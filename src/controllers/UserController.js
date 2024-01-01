f906978fa52bdb2608a7a8b2b4f82c5150d4dd40
NoeOn <alxg5516@gmail.com>
2023-12-31 21:04:36 -0600

    Agregando mas validaciones en mi UserValidator para el registro de usuarios

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -8,7 +8,7 @@ export async function registerUser(req, res) {
     }
     try {
         const user = await User.create(req.body);
-        res.status(200).json(user);
+        res.status(201).json(user);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
--- src/middlewares/UserValidator.js
+++ src/middlewares/UserValidator.js
@@ -2,21 +2,58 @@ import { body } from 'express-validator';
 import User from '../models/UserModel.js';
 
 export const registerUserValidations = [
+    body('name')
+        .trim()
+        .not()
+        .isEmpty()
+        .withMessage('Name is required')
+        .isLength({ min: 3 })
+        .withMessage('Name must be at least 3 characters long')
+        .matches(/^[a-z]+(\s[a-z]+)?$/i)
+        .withMessage('Name must only contain letters and a single space between words'),
+    body('lastname')
+        .trim()
+        .not()
+        .isEmpty()
+        .withMessage('Lastname is required')
+        .isLength({ min: 3 })
+        .withMessage('Lastname must be at least 3 characters long')
+        .matches(/^[a-z]+(\s[a-z]+)?$/i)
+        .withMessage('Lastname must only contain letters and a single space between words'),
+    body('birthdate')
+        .trim()
+        .not()
+        .isEmpty()
+        .withMessage('Birthdate is required')
+        .isDate()
+        .withMessage('Please enter a valid date')
+        .custom((value) => {
+            const birthdate = new Date(value);
+            const today = new Date();
+            const twelveYearsAgo = new Date(today.getFullYear() - 12, today.getMonth(), today.getDate());
+            if (birthdate > twelveYearsAgo) {
+                throw new Error('You must be at least 12 years old to register');
+            }
+        }),
     body('email')
+        .trim()
+        .not()
+        .isEmpty()
+        .withMessage('Email is required')
+        .normalizeEmail()
         .isEmail()
         .withMessage('Please enter a valid email')
         .custom(async (value) => {
-            try {
-                const user = await User.findOne({ where: { email: value } });
-                if (user) {
-                    return Promise.reject('Email already in use');
-                }
-            } catch (error) {
-                console.log(error);
+            const user = await User.findOne({ where: { email: value } });
+            if (user) {
+                return Promise.reject('Email already in use');
             }
         }),
     body('password')
         .trim()
+        .not()
+        .isEmpty()
+        .withMessage('Password is required')
         .isLength({ min: 8 })
         .withMessage('Password must be at least 8 characters long'),
 ]
