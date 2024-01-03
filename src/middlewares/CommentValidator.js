073e23175e69cf49f58a74e3b842eb9624b7ce2e
NoeOn <alxg5516@gmail.com>
2024-01-03 02:56:47 -0600

    Refactorizando el codigo de CommentRoutes.js y eliminando bloque de codigo duplicado de CommentValidator.js

--- src/middlewares/CommentValidator.js
+++ src/middlewares/CommentValidator.js
@@ -1,16 +1,6 @@
 import { body } from 'express-validator';
 
-export const commentCreateValidator = [
-    body('content')
-        .trim()
-        .not()
-        .isEmpty()
-        .withMessage('Content is required')
-        .isLength({ min: 5 })
-        .withMessage('Content must be at least 5 characters long'),
-];
-
-export const commentUpdateValidator = [
+export const commentValidator = [
     body('content')
         .trim()
         .not()
--- src/routes/CommentRoutes.js
+++ src/routes/CommentRoutes.js
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import { addCommentToPoster, getCommentsFromPoster, updateComment } from '../controllers/CommentController.js';
-import { commentCreateValidator, commentUpdateValidator } from '../middlewares/CommentValidator.js';
+import { commentValidator } from '../middlewares/CommentValidator.js';
 import { commentExists } from '../middlewares/CommentMiddeware.js';
 import { authenticateToken } from '../middlewares/AuthorizationToken.js';
 
 const router = Router();
 
-router.post('/add/:id', authenticateToken, commentCreateValidator, addCommentToPoster);
-router.put('/:id', authenticateToken, commentExists, commentUpdateValidator, updateComment);
+router.post('/add/:id', authenticateToken, commentValidator, addCommentToPoster);
+router.put('/:id', authenticateToken, commentExists, commentValidator, updateComment);
 router.get('/:posterId', getCommentsFromPoster);
 
 export default router;
