f9fd2a766bb60eaeaa1833722ec585ba62c98dce
NoeOn <alxg5516@gmail.com>
2024-01-05 00:39:02 -0600

    Agregando la cors a mi archivo server.js para poder recibir peticiones otro dominio

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import connect from './database/connection.js';
 import indexRoutes from './routes/indexRoutes.js';
 
@@ -10,6 +11,7 @@ async function startServer() {
     try {
         console.clear();
         await connect();
+        app.use(cors());
         app.use(express.json());
         app.use(indexRoutes);
         app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
